f3014b8f081002b036c2774b03b6e06188511369
Eli Morris-Heft <eli.morris.heft@gmail.com>
2013-04-17 11:55:01 -0500

    Fixed issue #8. Disabled options disable the whole row.

--- _controls/downloadCustomizer.js
+++ _controls/downloadCustomizer.js
@@ -68,9 +68,10 @@ can.Control('CanJSUS.DownloadCustomizer', {
 	_libraryChanged: function(libraryID) {
 		var self = this;
 		can.each(this.options.configuration.modules, function(module) {
-			var disallowed = module.configurations && module.configurations.indexOf(libraryID) < 0;
+			var disallowed = !!(module.configurations && module.configurations.indexOf(libraryID) < 0);
 			var check = $('#' + module.id);
 			check
+				.closest('tr').toggleClass('inactive', disallowed).end()
 				.prop('disabled', disallowed)
 				.prop('checked', disallowed ? false : check.prop('checked') || self.checkAlls[module.type]).change();
 		});
--- _controls/heroDownloadCustomizer.js
+++ _controls/heroDownloadCustomizer.js
@@ -9,4 +9,15 @@ CanJSUS.DownloadCustomizer('CanJSUS.HeroDownloadCustomizer', {
 	'select[name=configuration] change': function(el, ev) {
 		this._libraryChanged(el.val());
 	},
+	_libraryChanged: function(libraryID) {
+		var self = this;
+		can.each(this.options.configuration.modules, function(module) {
+			var disallowed = !!(module.configurations && module.configurations.indexOf(libraryID) < 0);
+			var check = $('#' + module.id);
+			check
+				.closest('li').toggleClass('inactive', disallowed).end()
+				.prop('disabled', disallowed)
+				.prop('checked', disallowed ? false : check.prop('checked') || self.checkAlls[module.type]).change();
+		});
+	}
 });
