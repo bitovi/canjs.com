eb53cb15d41214c05266ca30950331897a9fd149
Eli Morris-Heft <eli.morris.heft@gmail.com>
2013-04-18 13:48:26 -0500

    Added loading for community tab per Tom's request.

--- _controls/communityTabs.js
+++ _controls/communityTabs.js
@@ -16,24 +16,32 @@ can.Control('CanJSUS.CommunityTabs', {
 		this.state = new can.Observe({});
 		var self = this;
 
+		this.modelRequests = 6;
+		$('.loading').addClass('active');
 		CanJSUS.ForumPost.findAll().done(function(posts) {
+			self._hideLoading();
 			self.state.attr('forumPosts', posts);
 		});
 		// Missing counts for forum categories
 		CanJSUS.ChatLine.findAll().done(function(lines) {
+			self._hideLoading();
 			self.state.attr('lines', lines);
 		});
 		CanJSUS.Plugin.findAll().done(function(plugins) {
+			self._hideLoading();
 			self.state.attr('plugins', plugins);
 		});
 		// Missing counts for plugins/apps/articles
 		CanJSUS.Tweet.findAll().done(function(tweets) {
+			self._hideLoading();
 			self.state.attr('tweets', tweets);
 		});
 		CanJSUS.GithubIssue.findAll().done(function(issues) {
+			self._hideLoading();
 			self.state.attr('issues', issues);
 		});
 		CanJSUS.GithubEvent.findAll().done(function(commits) {
+			self._hideLoading();
 			self.state.attr('commits', commits);
 		});
 		// Missing follower counts for github
@@ -55,5 +63,11 @@ can.Control('CanJSUS.CommunityTabs', {
 			.filter('.' + selectedTab).addClass('active');
 		var tabControl = this.options.tabControls[selectedTab];
 		new tabControl($('.content > .container'), {state: this.state});
+	},
+	_hideLoading: function() {
+		this.modelRequests--;
+		if(this.modelRequests < 1) {
+			$('.loading').removeClass('active');
+		}
 	}
 });
