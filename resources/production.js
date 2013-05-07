82da3bf2dfb6b46e3841b35ed4fef8310154349c
Eli Morris-Heft <eli.morris.heft@gmail.com>
2013-05-07 14:32:58 -0500

    Live example works.

--- resources/production.js
+++ resources/production.js
@@ -7997,8 +7997,8 @@ CanJSUS.CommunityTab('CanJSUS.IssuesTab', {
 	}
 });
 can.Control('CanJSUS.LiveExample', {
-	defaults: {
-		code: "var Todo = can.Model({\n\
+    defaults: {
+        code: "var Todo = can.Model({\n\
   findAll: 'GET /todos',\n\
   findOne: 'GET /todos/{id}',\n\
   create:  'POST /todos',\n\
@@ -8008,84 +8008,78 @@ can.Control('CanJSUS.LiveExample', {
 \n\
 var TodoList = can.Control({\n\
   init: function() {\n\
+    this.options = new can.Observe(this.options);\n\
+    \n\
+//  fetch data from the server\n\
+    Todo.findAll()\n\
+      .done(can.proxy(function(todos) {\n\
+        this.options.attr('todos', todos);\n\
+      }, this));\n\
+    \n\
+//  render the template & append it to the page\n\
     this.element.html(can.view(\n\
       'todoList',\n\
-      {todos: this.options.todos}\n\
+      this.options\n\
     ));\n\
   }\n\
 });\n\
 \n\
-new TodoList('#todos', {todos: Todo.findAll()});",
-		template: "{{! in a script tag with id 'todoList'}}\n\
+new TodoList('#todos', {todos: []});",
+        template: "{{! in a script tag with id 'todoList'}}\n\
 <h3>Today's To-dos</h3>\n\
 <ul>\n\
 {{#todos}}\n\
   <li>{{description}}</li>\n\
 {{/todos}}\n\
 </ul>",
-		todos: [
-			'Wash the dishes',
-			'Spring cleaning',
-			'Wash the car',
-			'Go grocery shopping',
-			'Balance checkbook',
-			'Clear inbox',
-			'Build perpetual motion device',
-			'Harness cold fusion',
-			'Mow the lawn',
-			'Do the laundry',
-			'Renew library books',
-			'Pay bills'
-		]
-	}
+        todos: [
+            'Wash the dishes',
+            'Spring cleaning',
+            'Wash the car',
+            'Go grocery shopping',
+            'Balance checkbook',
+            'Clear inbox',
+            'Build perpetual motion device',
+            'Harness cold fusion',
+            'Mow the lawn',
+            'Do the laundry',
+            'Renew library books',
+            'Pay bills'
+        ]
+    }
 }, {
-	init: function() {
-		$('.code', this.element).val(this.options.code);
-		$('.template', this.element).val(this.options.template);
+    init: function() {
+        $('.code', this.element).val(this.options.code);
+        $('.template', this.element).val(this.options.template);
 
-		this.registerFixtures();
-	},
-	'.execute click': 'runExample',
-	runExample: function(el, ev) {
-		var code = $('.code', this.element).val(),
-			template = $('.template', this.element).val();
+        this.registerFixtures();
+        //this.runExample();
+    },
+    '.execute click': 'runExample',
+    runExample: function(el, ev) {
+        var code = $('.code', this.element).val(),
+            template = $('.template', this.element).val();
 
-		// empty the result area
-		$('#todos', this.element).empty();
+        // empty the result area
+        $('#todos', this.element).empty();
 
-		try {
-			// register the template
-			can.view.mustache('todoList', template);
-			
-			//call the code
-			(new Function(code))();
-		} catch (e) { }
-	},
-	registerFixtures: function() {
-		/*var randomInt = function(min, max) {
-			return Math.round((Math.random() * (max - min)) + min);
-		};
+        // register the template
+        can.view.mustache('todoList', template);
+            
+        //call the code
+        (new Function(code))();
+    },
+    registerFixtures: function() {
+        var store = can.fixture.store(this.options.todos.length, can.proxy(function(ix) {
+            return {
+                id: ix+1,
+                description: this.options.todos[ix]
+            };
+        }, this));
 
-		can.fixture("GET /todos", can.proxy(function(original, respondWith, settings) {
-			var start = randomInt(0, this.options.todos.length - 1),
-				end = randomInt(start + 1, this.options.todos.length);
-			respondWith(this.options.todos.slice(start, end));
-		}, this));
-
-		can.fixture("GET /todos/{id}", can.proxy(function(original, respondWith, settings) {
-			respondWith(this.options.todos[randomInt(0, this.options.todos.length - 1)]);
-		}, this));
-*/
-		var store = can.fixture.store(this.options.todos.length, can.proxy(function(ix) {
-			return {
-				id: ix+1,
-				description: this.options.todos[ix]
-			};
-		}, this));
-
-		can.fixture('GET /todos', store.findAll);
-		can.fixture('GET /todos/{id}', store.findOne);
-	}
+        can.fixture('GET /todos', store.findAll);
+        can.fixture('GET /todos/{id}', store.findOne);
+    }
 });
 can.Mustache.registerHelper('makeHref', function(src) {
 	return src().replace(/ /g, "_")
