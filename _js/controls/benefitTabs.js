81e479e8d67c3338187f85c9108544b1de821d5b
Eli Morris-Heft <eli.morris.heft@gmail.com>
2013-05-09 12:33:20 -0500

    Revamped benefit tabs to be more flexible and accomodating given upcoming possible revisions. Made 'powerful' the default. Added a 'more' link. Hits issue #51.

--- _js/controls/benefitTabs.js
+++ _js/controls/benefitTabs.js
@@ -1,27 +1,49 @@
 can.Control('CanJSUS.Benefits', {
 	defaults: {
-		taglines: {
-			'flexible': 'Use it anywhere. Works with your libraries, plugins, and templates.',
-			'powerful': 'Strong enough for high-power applications. Simple enough to learn in a day.',
-			'fast': 'Sleek, lightweight, and responsive: exactly what it says on the can.'
+		tabs: {
+			flexible: {
+				className: 'flexible',
+				title: 'Flexible',
+				tagline: 'Works with jQuery, Dojo, Mootools, YUI, and Zepto. Reuse your existing templates.',
+				link: 'guides/Why.html#Flexible'
+			},
+			powerful: {
+				className: 'powerful',
+				title: 'Powerful',
+				tagline: 'Packs in everything you need to build your app. And you can learn it in a day.',
+				link: 'guides/Why.html#Powerful'
+			},
+			fast: {
+				className: 'fast',
+				title: 'Fast',
+				tagline: 'Sleek, responsive, and only 36K: exactly what it says on the can.',
+				link: 'guides/Why.html#Fast'
+			}
 		}
 	}
 }, {
 	init: function() {
-		this.state = new can.Observe({selectedTab: 'flexible', tagline: this.options.taglines.flexible});
-		this.element.html(can.view('templates/benefitTabs.mustache', this.state));
-		this._switchBenefit($('li[data-benefit=' + this.state.selectedTab + ']', this.element));
+		this.state = new can.Observe({tabs: this.options.tabs, selectedTab: this.options.tabs.powerful});
+		this.element.html(can.view('templates/benefitTabs.mustache', this.state, {
+			makeTabs: function(tabs, options) {
+				var out = '';
+				can.each(tabs().attr(), function(val, key) {
+					out += options.fn(val);
+				});
+				return out;
+			}
+		}));
+		this._switchBenefit('powerful');
 	},
-	'li mouseover': '_switchBenefit',
-	'li click': '_switchBenefit',
-	_switchBenefit: function(el) {
-		this.state.attr({
-			'selectedTab': can.data(el, 'benefit') || '',
-			'tagline': this.options.taglines[can.data(el, 'benefit')] || ''
-		});
+	'li mouseover': function(el, ev) {
+		console.log(el.data('benefit'));
+		this._switchBenefit(el.data('benefit'));
+	},
+	_switchBenefit: function(benefit) {
+		this.state.attr('selectedTab', this.options.tabs[benefit]);
 
 		$('li', this.element).removeClass('active');
-		el.addClass('active');
+		$('li[data-benefit=' + benefit + ']', this.element).addClass('active');
 
 	}
 });
