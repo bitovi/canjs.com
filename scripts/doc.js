94e131e9e9a292aa54318a9ff380a2a16502927d
David Luecke <daff@neyeon.com>
2013-06-25 16:34:56 -0600

    Initial setup for using Rhino DocumentJS

---
+++ scripts/doc.js
@@ -0,0 +1,28 @@
+load('steal/rhino/rhino.js');
+
+steal("documentjs", "steal/rhino/json.js", function (DocumentJS) {
+	var pkg = JSON.parse(readFile('./can/package.json'));
+	var self = JSON.parse(readFile('./package.json'));
+	var builder = JSON.parse(readFile('./can/builder.json'));
+
+	DocumentJS('scripts/doc.html',{
+		"markdown": [ 'can' ],
+		"out": "docs",
+		"parent": "canjs",
+		// "static": "documentjs/site/static",
+		"root": '',
+		"package": pkg,
+		"self": self,
+		"builder": builder,
+		// helpers: handlebarsHelpers,
+		"url": {
+			builderData: 'http://bitbuilder.herokuapp.com/canjs',
+			builder: 'http://bitbuilder.herokuapp.com/can.custom.js',
+			bithub: 'http://api.bithub.com/api/events/',
+			cdn: '//canjs.com/release/'
+		},
+		// TODO move out (possibly make a flag or something)
+		"layout": "shared/_templates/page.mustache",
+		"docs": "shared/_templates/docs.mustache"
+	});
+});
