80ec47e1884f483ed2b5de8f73f9ee1f38eeee56
Justin Meyer <justinbmeyer@gmail.com>
2013-07-18 17:17:49 -0500

    latest updates

--- scripts/documentation.js
+++ scripts/documentation.js
@@ -1,1 +1,17 @@
-steal('can');
+steal('can/construct',
+	'can/construct/proxy',
+	'can/control',
+	'can/control/route',
+	'can/control/plugin',
+	'can/model',
+	'can/observe',
+	'can/observe/attributes',
+	'can/observe/backup',
+	'can/observe/delegate',
+	'can/observe/validations',
+	'can/route',
+	'can/view/ejs',
+	'can/util/fixture',
+	'can/view/modifiers',
+	'can/view/mustache',
+	'can/util/func.js')
