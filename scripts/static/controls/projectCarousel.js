1482e80a20092588c88e34a02d944e345cf9ae9a
Josh Dean <imjoshdean@me.com>
2013-09-12 14:00:25 -0500

    Next iteration of project carousel start.

--- scripts/static/controls/projectCarousel.js
+++ scripts/static/controls/projectCarousel.js
@@ -14,8 +14,12 @@ can.Control('Bitovi.OSS.ProjectCarousel', {
       return text.substr(0, 400) + (text.length > 400 ? '...' : '');
     });
 
-    Bitovi.OSS.App.findAll({limit: 10, size: 'canjscom'}).done(can.proxy(function(apps) {
-      this.state.attr('apps', apps);
+    Bitovi.OSS.App.findAll({limit: 300, size: 'canjscom'}).done(can.proxy(function(apps) {
+      var len = apps.length;
+      this.state.attr('length', len);
+      var rowOne = apps.splice(0, (len / 2));
+      this.state.attr('appRowOne', rowOne);
+      this.state.attr('appRowTwo', apps);
       this.state.attr('current', {
         title: apps[0].title,
         body: apps[0].body,
@@ -24,37 +28,43 @@ can.Control('Bitovi.OSS.ProjectCarousel', {
 
       this.element.html(can.view(this.options.view, this.state));
 
-      this.element.find('.carousel').scrollbox({
-        direction: 'h',
-        switchItems: 1,
-        distance: 200,
-        delay: 10
-      });
-
       this.current = this.element.find('li:first').addClass('current');
     }, this));
   },
 
-  '.carousel moving': function(el, ev, direction) {
-    var index = direction == 'forward' ? 1 : 0;
-
-    this.showProject(el.find('li:eq(' + index + ')'), ev);
-  },
-
-//  'li mouseenter': 'showProject',
-
-  'a click': function(el, ev) {
+  'a:not(.button) click': function(el, ev) {
+    return false;
     el = el.closest('li');
     this.showProject(el, ev);
-    return false;
   },
 
   '.prev click' : function(el, ev) {
-    this.element.find('.carousel').trigger('backward');
+    this.move('right');
   },
 
   '.next click' : function(el, ev) {
-    this.element.find('.carousel').trigger('forward');
+    this.move('left');
+  },
+
+  move: function(direction) {
+    var left = (direction === 'left' ? '+=' : '-=') + '242px',
+      element = (direction === 'left' ? ':last' : ':first'),
+      insert = direction === 'left' ? 'prepend' : 'append',
+      self = this;
+
+    if(this.animating) {
+      return;
+    }
+    
+    this.animating = true;
+    this.element.find('.list').animate({
+      left: left
+    }, 500, function() {
+      var $this = $(this),
+        li = $this.find('li').filter(element);
+      $this[insert](li).css('left', '');
+      self.animating = false;
+    });
   },
 
   showProject: function(el, ev) {
