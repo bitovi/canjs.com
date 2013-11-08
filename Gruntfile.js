0e9a87ed2e37b191a88729ee6c5cd7ea82038a82
David Luecke <daff@neyeon.com>
2013-11-08 13:32:48 -0700

    Gruntfile for building and copying distributable assets.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,36 @@
 module.exports = function(grunt) {
+  grunt.registerTask('version', 'Synchronizes source repositories package.json version with bower.json', function() {
+
+  });
+
   grunt.initConfig({
     clean: {
-      distributables: ['**/*.js', '!Gruntfile.js']
+      distributables: ['**/*.js', '!Gruntfile.js', 'amd/', 'steal/', '!**/node_modules/**']
+    },
+    copy: {
+      dist: {
+        expand: true,
+        cwd: '../canjs/dist/',
+        src: ['**'],
+        dest: ''
+      }
+    },
+    shell: {
+      build: {
+        command: 'grunt build',
+        options: {
+          execOptions: {
+            stdout: true,
+            cwd: __dirname + '/../canjs/'
+          }
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('default', ['shell:build', 'clean', 'copy']);
 }
