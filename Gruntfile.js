65df751875d3a49f0cc387505c6d83b03f7622af
David Luecke <daff@neyeon.com>
2013-11-11 15:07:10 -0700

    Finalizing release task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,14 @@
 module.exports = function(grunt) {
-  grunt.registerTask('version', 'Synchronizes source repositories package.json version with bower.json', function() {
+  grunt.registerTask('syncVersion', 'Synchronizes source repositories package.json version with bower.json', function() {
+    var options = this.options();
+    var fromData = grunt.file.readJSON(options.from);
+    var toData = grunt.file.readJSON(options.to);
 
+    grunt.log.writeln('Writing version ' + fromData.version + ' to ' + options.to);
+
+    toData.version = fromData.version;
+
+    grunt.file.write(options.to, JSON.stringify(toData, null, '  '));
   });
 
   grunt.initConfig({
@@ -24,6 +32,23 @@ module.exports = function(grunt) {
             cwd: __dirname + '/../canjs/'
           }
         }
+      },
+      release: {
+        command: function() {
+          var version = grunt.file.readJSON('bower.json').version;
+
+          return 'git add . --all && ' +
+            'git commit -m "v' + version + '" && ' +
+            'git push origin master && ' +
+            'git tag ' + version + ' && ' +
+            'git push origin --tags';
+        }
+      }
+    },
+    syncVersion: {
+      options: {
+        from: '../canjs/package.json',
+        to: 'bower.json'
       }
     }
   });
@@ -32,5 +57,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('default', ['shell:build', 'clean', 'copy']);
+  grunt.registerTask('default', ['shell:build', 'clean', 'copy', 'syncVersion', 'release']);
 }
