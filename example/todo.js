18df1eba9abcee28e5cd04ef45e77da7aa17c73b
Josh Dean <imjoshdean@me.com>
2013-11-14 14:57:28 -0600

    Update docs for 2.0.2

--- example/todo.js
+++ example/todo.js
@@ -8,96 +8,61 @@ var Todo = can.Model({
 	destroy: 'DELETE /todos/{id}'
 }, {});
 
-// This controller manages our to-do list. It will
-// instantiate the view and respond to events that are
-// triggered in the element the control is bound to.
-var TodoBoard = can.Control({
-	init: function() {
-		var el = this.element;
-        // Go get all the to-dos and
-        // feed them to our template.
-        // (See the script tag with id="todoList"
-        // for the mustache template being used.)
-        this.element.html(can.view('todoBoard', new Todo.List({})));
-	},
-    // When a checkbox's value changes...
-	'input[type=checkbox] change': function(el, ev) {
-        // ...update the 'done' property on the
-        // corresponding to-do and save to the server.
-        el.parent().data('todo').attr('done', el.prop('checked')).save();
-	},
-    // When the delete button is clicked...
-	'.delete click': function(el, ev) {
-        // ...destroy the corresponding to-do on the server.
-        // The template will re-render itself and the
-        // deleted to-do will be removed.
-		el.parent().data('todo').destroy();
-	},
-    // When the description is clicked...
-    '.description click': function(el, ev) {
-        // Change the page state (we're using the route)
-        // to indicate that we're editing that to-do.
-        can.route.attr('id', el.parent().data('todo').id);
+can.Component.extend({
+  tag: "todos-list",
+  template: can.view('todoListTemplate'),
+  scope: {
+    todos: new Todo.List({}),
+    select: function(todo){
+      can.route.attr("id",todo.attr("id"))
     }
+  },
+  helpers: {
+    todoClass: function(options){
+      if(options.context.attr('complete')) {
+        return "class='description done'"
+      }
+      else {
+        return "class='description'" 
+      }
+    }
+  }
 });
 
-var TodoEditor = can.Control({
-    loadTodo: function(todo) {
-        this.options.todo = todo;
-        // The to-do we're editing has changed, and
-        // there are events bound on the to-do, so we
-        // need to re-bind those events to the new to-do.
-        this.on();
-        // Load up the editor with the to-do
-        // that was passed in.
-        // (See the script tag with id="todoEditor"
-        // for the mustache template being used.)
-        this.element.html(can.view('todoEditor', this.options.todo));
-     },
-    // When the input changes...
-    'input change': function(el, ev) {
-        this.element.find('.spinner').removeClass('hidden');
-        // ...save the new description to the server...
-        this.options.todo.attr('description', el.val()).save();
-    },
-    '{todo} updated': function() {
-        // Remove the spinner when the todo is updated.
-        this.element.find('.spinner').addClass('hidden');
-    },
-    // If the todo we're editing is removed...
-    '{todo} destroyed': function() {
-        // ...change the page state (here, the route)
-        // to indicate that we are no longer editing.
-        can.route.removeAttr('id');
-    }
+can.Component.extend({
+  tag: "todos-editor",
+  template: can.view('todoEditorTemplate')
 });
 
-// Routing pulls the editor and the to-do board together
-// and takes care of routing as well.
-var Routing = can.Control({
-    init: function() {
-        // Declare what our routes will look like.
-        can.route('todos/:id');
-        // Fire up the to-do board.
-        new TodoBoard($('#board'));
-        // Also prepare our editor.
-        this.editor = new TodoEditor($('#editor'));
-        $('#editor').hide();
+can.Component.extend({
+  tag: "todos-app",
+  scope: {
+    todo: null
+  },
+  events: {
+    "{can.route} id": function(route, ev, id){
+      if(id){
+        var self = this;
+        Todo.findOne({id: id}, $.proxy(function(todo){
+          this.scope.attr("todo", todo)
+        }, this))
+      } else {
+        this.scope.removeAttr("todo")
+      }
     },
-    // When the route changes...
-    'todos/:id route': function(data) {
-        if(data.id) {
-            // ...if we're editing, go find that to-do and
-            // load it into the editor.
-            var editor = this.editor;
-            Todo.findOne({id: data.id}).done(function(todo) {
-                editor.loadTodo(todo);
-                editor.element.show();
-            });
-        } else {
-            // ...if we're not editing,
-            // hide the editor.
-            $('#editor').hide();
-        }
+    "{Todo} destroyed": function(Todo, ev, destroyedTodo){
+      if( destroyedTodo.id == can.route.attr("id") ){
+        can.route.removeAttr("id")
+      }
+    },
+    "{todo} change": function(todo, ev, attr){
+      if( attr === "description" || attr == "complete" ) {
+        todo.save();
+        can.route.removeAttr("id");
+      }
     }
-});
+  }
+})
+
+can.route("todos/:id");
+can.route.ready();
