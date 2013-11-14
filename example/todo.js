045eed5ae112c36e43a61dfad3037a811f1d288d
Josh Dean <imjoshdean@me.com>
2013-11-14 16:33:57 -0600

    Update indentations and such of example.

--- example/todo.js
+++ example/todo.js
@@ -1,7 +1,7 @@
 // Here's where we get, save, and delete to-dos.
 // (Yep, this is all we need to access the Todo
 // REST endpoints!)
-var Todo = can.Model({
+var Todo = can.Model.extend({
 	findAll: 'GET /todos',
     findOne: 'GET /todos/{id}',
 	update: 'PUT /todos/{id}',
@@ -9,56 +9,32 @@ var Todo = can.Model({
 }, {});
 
 can.Component.extend({
-  tag: "todos-list",
-  template: can.view('todoListTemplate'),
+  tag: "todos-app",
   scope: {
+    selectedTodo: null,
     todos: new Todo.List({}),
     select: function(todo){
       can.route.attr("id",todo.attr("id"))
+    },
+    change: function(todo) {
+        todo.save();
+        can.route.removeAttr("id");
     }
-  },
-  helpers: {
-    todoClass: function(options){
-      if(options.context.attr('complete')) {
-        return "class='description done'"
-      }
-      else {
-        return "class='description'" 
-      }
-    }
-  }
-});
-
-can.Component.extend({
-  tag: "todos-editor",
-  template: can.view('todoEditorTemplate')
-});
-
-can.Component.extend({
-  tag: "todos-app",
-  scope: {
-    todo: null
   },
   events: {
     "{can.route} id": function(route, ev, id){
       if(id){
         var self = this;
         Todo.findOne({id: id}, $.proxy(function(todo){
-          this.scope.attr("todo", todo)
+          this.scope.attr("selectedTodo", todo)
         }, this))
       } else {
-        this.scope.removeAttr("todo")
+        this.scope.removeAttr("selectedTodo")
       }
     },
-    "{Todo} destroyed": function(Todo, ev, destroyedTodo){
-      if( destroyedTodo.id == can.route.attr("id") ){
+    "{Todo} destroyed": function(Todo, ev, dsTodo){
+      if( dsTodo.id == can.route.attr("id") ){
         can.route.removeAttr("id")
-      }
-    },
-    "{todo} change": function(todo, ev, attr){
-      if( attr === "description" || attr == "complete" ) {
-        todo.save();
-        can.route.removeAttr("id");
       }
     }
   }
