2d903a74b51fc52b13c6e947d1535e59d70a94a1
Mihael Konjevic <konjevic@gmail.com>
2014-01-21 10:03:36 -0800

    Show pre release versions

--- scripts/static/controls/versionDownloadCustomizer.js
+++ scripts/static/controls/versionDownloadCustomizer.js
@@ -1,3 +1,12 @@
+var getBranchDownloads = function(){
+	return can.map(can.grep(docConfig.versions, function(v){ return !!v.branch }), function(v){
+		return {
+			name : v.number,
+			zipball_url : can.sub('https://github.com/bitovi/canjs/archive/{branch}.zip', v)
+		}
+	});
+}
+
 can.Control.extend('Bitovi.OSS.VersionDownloadCustomizer', {
 	defaults : {
 		view : 'docs/static/templates/versionDownloadCustomizer.mustache'
@@ -7,6 +16,8 @@ can.Control.extend('Bitovi.OSS.VersionDownloadCustomizer', {
 		this.leftDownloads = new can.List;
 		this.rightDownloads = new can.List;
 		this.isOpen = false;
+
+		this.branchDownloads = getBranchDownloads();
 
 		this.element.find('#version-download-customizer').replaceWith(can.view(this.options.view, {
 			leftDownloads : this.leftDownloads,
@@ -20,9 +31,11 @@ can.Control.extend('Bitovi.OSS.VersionDownloadCustomizer', {
 		});
 	},
 	renderResults : function(data){
-		var downloads = data.data,
+		this.branchDownloads.push.apply(this.branchDownloads, data.data);
+
+		var downloads = this.branchDownloads,
 			length = downloads.length,
-			half = parseInt(length / 2, 10);
+			half = Math.ceil(length / 2);
 
 		this.leftDownloads.replace(downloads.slice(0, half));
 		this.rightDownloads.replace(downloads.slice(half, length));
