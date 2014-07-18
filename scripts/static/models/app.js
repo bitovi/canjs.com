3eeccd318ae923fedf9a0457395491faa8de24ad
Mihael Konjevic <konjevic@gmail.com>
2014-07-18 20:37:19 +0200

    Use the correct urls for the project carousel images

--- scripts/static/models/app.js
+++ scripts/static/models/app.js
@@ -1,8 +1,14 @@
+var __imageUrl = function(url){
+  var arr = url.split('/');
+  arr[arr.length - 1] = "232x147_" + arr[arr.length - 1];
+  return arr.join('/');
+}
+
 can.Model("Bitovi.OSS.App", {
   model: function(data) {
     return {
       title: data.title,
-      picture: data.image_url,
+      picture: __imageUrl(data.original_image_url),
       body: data.body,
       link: data.url.indexOf('http') === -1 ? 'http://' + data.url : data.url
     };
