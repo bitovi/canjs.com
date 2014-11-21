f0f8694f7ae055d108065283162a47de9dc3db16
David Luecke <daff@neyeon.com>
2014-11-21 15:26:20 -0700

    Updating release

--- release/latest/amd/can/util/yui.js
+++ release/latest/amd/can/util/yui.js
@@ -1,8 +1,8 @@
 /*!
- * CanJS - 2.1.3
+ * CanJS - 2.1.4
  * http://canjs.us/
  * Copyright (c) 2014 Bitovi
- * Mon, 25 Aug 2014 21:51:29 GMT
+ * Fri, 21 Nov 2014 22:25:48 GMT
  * Licensed MIT
  * Includes: CanJS default build
  * Download from: http://canjs.us/
