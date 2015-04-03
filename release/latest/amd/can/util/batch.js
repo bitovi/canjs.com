79406b3513d683e9f25b652e764cd4ec5fbdb8a9
David Luecke <daff@neyeon.com>
2015-04-03 17:37:49 -0600

    Updating release v2.2.4

--- release/latest/amd/can/util/batch.js
+++ release/latest/amd/can/util/batch.js
@@ -1,12 +1,12 @@
 /*!
- * CanJS - 2.2.3
+ * CanJS - 2.2.4
  * http://canjs.com/
  * Copyright (c) 2015 Bitovi
- * Fri, 03 Apr 2015 15:31:35 GMT
+ * Fri, 03 Apr 2015 23:27:46 GMT
  * Licensed MIT
  */
 
-/*can@2.2.3#util/batch/batch*/
+/*can@2.2.4#util/batch/batch*/
 define(['can/util/can'], function (can) {
     var batchNum = 1, transactions = 0, batchEvents = [], stopCallbacks = [], currentBatchEvents = null;
     can.batch = {
